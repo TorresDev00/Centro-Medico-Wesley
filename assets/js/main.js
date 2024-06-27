7e02492db19df92ac6113de8e62664752c764ad2
enmanuel <enmanuel.josue.torres13@gmail.com>
2024-06-27 02:01:03 -0400

    feet: change index maquetado completo

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const swiper = new Swiper('.servicios', {
+        speed: 600,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            bulletClass: 'swiper-pagination-bullet',
+            bulletActiveClass: 'swiper-pagination-bullet-active',
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true
+        },
+        grabCursor: true,
+        mousewheel: true,
+        touchRatio: 0.5,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 1,
+            },
+            1200: {
+                slidesPerView: 1,
+
+            }
+        }
+    });
+
+})
