59b7b6b6a827c4ef95b585526e4860c05df96448
enmanuel <enmanuel.josue.torres13@gmail.com>
2024-07-24 01:07:50 -0400

    change js Breakpoint

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,7 +107,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     document.addEventListener('show.bs.modal', function (event) {
-        document.body.style.paddingRight = '16px'; // Ajusta el valor según el ancho de la barra de desplazamiento
+        if(window.innerWidth > 992){
+         document.body.style.paddingRight = '16px'; // Ajusta el valor según el ancho de la barra de desplazamiento
+        }
     });
     
     document.addEventListener('hidden.bs.modal', function (event) {
