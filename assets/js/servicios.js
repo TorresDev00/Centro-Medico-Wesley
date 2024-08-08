b95d5b9c63a7e10a01a7ac3b9678ce338cbc155f
Torres <enmanuel.josue.torres13@gmail.com>
2024-08-08 14:03:31 -0400

    feet:  change interactive maps by location

--- assets/js/servicios.js
+++ assets/js/servicios.js
@@ -45,42 +45,58 @@ const infoSedes = {
   sede_1: {
       nombre: 'Cabudare',
       direccion: 'C. Altamira, Cabudare 3023, Lara',
-      maps: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d337.4004379242522!2d-69.2224774983544!3d10.008255855299405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f000a08eec9%3A0x4194a391889eeae9!2sCentro%20Medico%20Wesley%20Cabudare!5e0!3m2!1ses-419!2sve!4v1723052696696!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+      maps: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d337.4004379242522!2d-69.2224774983544!3d10.008255855299405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f000a08eec9%3A0x4194a391889eeae9!2sCentro%20Medico%20Wesley%20Cabudare!5e0!3m2!1ses-419!2sve!4v1723052696696!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+      numberSede: 'sede_1'
   },
   sede_2: {
       nombre: 'Valle Dorado',
       direccion: 'Sede 2',
-      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;"allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;"allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+      numberSede: 'sede_2'
   },
   sede_3: {
       nombre: 'La Rotaria',
       direccion: 'Sede 3',
-      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+      numberSede: 'sede_3'
   },
   sede_4: {
       nombre: 'Tejeria',
       direccion: 'Sede 4',
-      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+      numberSede: 'sede_4'
   },
   sede_5: {
       nombre: 'La Victoria',
       direccion: 'Sede 5',
-      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+      numberSede: 'sede_5'
   },
   sede_6: {
       nombre: 'Coro',
       direccion: 'Sede 6',
-      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+      maps: '<iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d491.13823052044654!2d-69.22253736994304!3d10.008115351027937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e875f722e50bdc7%3A0x68fa4d525677f0bb!2sSeminario%20Wesleyano%20de%20Venezuela!5e0!3m2!1ses-419!2sve!4v1721919468022!5m2!1ses-419!2sve" style="border:0; width: 100%; height: 500px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+      numberSede: 'sede_6'
   }
 };
 
 // Sedes con sub-sedes
-const sedesPorSede = {
-  sede_1: ['Cabudare', 'Valle Dorado', 'La Rotaria'],
-  sede_2: ['Coro'], // Aquí se incluye "Coro" si es que tiene sub-sedes
-  sede_3: ['Tejeria', 'La Victoria'],
-  
+const sedesPorEstado = {
+  sede_lara: ['Cabudare', 'Valle Dorado', 'La Rotaria'],
+  sede_falcon: ['Coro'], 
+  sede_aragua: ['Tejeria', 'La Victoria'],
 };
+
+
+function buscarSede(nombre){
+  for(const key in infoSedes){
+    if(infoSedes[key].nombre.toLowerCase() === nombre.toLowerCase()){
+      return infoSedes[key];
+    }
+  }
+  return null
+}
+
 // Seleccionamos los íconos de las sedes
 const locationIcons = document.querySelectorAll('.location-icon');
 
@@ -88,9 +104,6 @@ locationIcons.forEach(icon => {
   icon.addEventListener('click', function () {
       // Obtenemos la sede seleccionada
       const sedeSeleccionada = this.getAttribute('data-sede');
-
-      // Obtenemos las especialidades para la sede seleccionada
-      const especialidades = especialidadesPorSede[sedeSeleccionada] || [];
 
       // Obtenemos la información de la sede seleccionada
       const info = infoSedes[sedeSeleccionada] || {};
@@ -104,12 +117,19 @@ locationIcons.forEach(icon => {
           service.classList.remove('visible'); // Ocultamos los servicios
       });
 
-      if (sedeSeleccionada === 'sede_2') {
+      if (sedeSeleccionada === 'sede_falcon') {
           // Mostrar directamente la información de Coro
+
+          let estado = sedesPorEstado[sedeSeleccionada];
+
+          const sedeCoro = buscarSede(estado[0])
+
+          let especialidades = especialidadesPorSede[sedeCoro.numberSede]
+
           tituloStart.innerHTML = `
-              <div id="maps" style="position: relative; overflow: hidden;">${info.maps}</div>
-              <h2 class="m-3 text-1 fs-3"><i class="bi bi-hospital"></i> <span id="sedeNombre">Sede <strong class="text-darkCyan">${info.nombre}</strong></span></h2>
-              <h2 class="m-3 text-1 fs-3"><i class="bi bi-geo-alt-fill"></i> Dirección: <span id="sedeDireccion"><strong class="text-darkCyan">${info.direccion}</strong></span></h2>
+              <div id="maps" style="position: relative; overflow: hidden;">${sedeCoro.maps}</div>
+              <h2 class="m-3 text-1 fs-3"><i class="bi bi-hospital"></i> <span id="sedeNombre">Sede <strong class="text-darkCyan">${sedeCoro.nombre}</strong></span></h2>
+              <h2 class="m-3 text-1 fs-3"><i class="bi bi-geo-alt-fill"></i> Dirección: <span id="sedeDireccion"><strong class="text-darkCyan">${sedeCoro.direccion}</strong></span></h2>
               <h2 class="m-3 text-1 fs-3"><i class="bi bi-capsule"></i> Esta sede ofrece los siguientes servicios para tu <strong class="text-darkCyan">bienestar:</strong></h2>
           `;
           scrollToInfo(tituloStart);
@@ -121,38 +141,45 @@ locationIcons.forEach(icon => {
                   service.classList.add('visible'); // Añadimos la clase visible
               }
           });
-      } else if (sedesPorSede[sedeSeleccionada]) {
-          // Crear el menú de sub-sedes
-          let menu = '<div class="menuSedes"><ul class="list-group">';
-          sedesPorSede[sedeSeleccionada].forEach(subSede => {
-              menu += `<li class="list-group-item sedeItem">${subSede}</li>`;
-          });
-          menu += '</ul></div>';
-          tituloStart.innerHTML = menu;
-          scrollToInfo(tituloStart);
+      } else if (sedesPorEstado[sedeSeleccionada]) {
+        // Crear el menú de sub-sedes
+        let menu = '<div class="menuSedes"><ul class="list-group">';
+        sedesPorEstado[sedeSeleccionada].forEach(subSede => {
+            menu += `<li class="list-group-item sedeItem fs-4"><i class="bi bi-hospital fs-4"></i> ${subSede}</li>`;
+        });
+        menu += '</ul></div>';
+        const tituloStart = document.getElementById('tituloStart');
+        tituloStart.innerHTML = menu;
+        scrollToInfo(tituloStart);
 
-          // Añadir evento a cada sub-sede para mostrar la información
-          document.querySelectorAll('.sedeItem').forEach(item => {
-              item.addEventListener('click', function () {
-                  const subSedeNombre = this.innerText;
-                  tituloStart.innerHTML = `
-                      <div id="maps" style="position: relative; overflow: hidden;">${info.maps}</div>
-                      <h2 class="m-3 text-1 fs-3"><i class="bi bi-hospital"></i> <span id="sedeNombre">Sede <strong class="text-darkCyan">${subSedeNombre}</strong></span></h2>
-                      <h2 class="m-3 text-1 fs-3"><i class="bi bi-geo-alt-fill"></i> Dirección: <span id="sedeDireccion"><strong class="text-darkCyan">${info.direccion}</strong></span></h2>
-                      <h2 class="m-3 text-1 fs-3"><i class="bi bi-capsule"></i> Esta sede ofrece los siguientes servicios para tu <strong class="text-darkCyan">bienestar:</strong></h2>
-                  `;
-                  scrollToInfo(tituloStart);
+        // Añadir evento a cada sub-sede para mostrar la información
+        document.querySelectorAll('.sedeItem').forEach(item => {
+            item.addEventListener('click', function () {
+                const subSedeNombre = this.textContent.trim();
+                const subSede = buscarSede(subSedeNombre);
+                
+                if (subSede) {
+                    const especialidades = especialidadesPorSede[subSede.numberSede] || [];
 
-                  // Mostrar los servicios de la sub-sede seleccionada
-                  services.forEach(service => {
-                      const serviceType = service.getAttribute('data-service');
-                      if (especialidades.includes(serviceType)) {
-                          service.classList.add('visible'); // Añadimos la clase visible
-                      }
-                  });
-              });
-          });
-      } else {
+                    tituloStart.innerHTML = `
+                        <div id="maps" style="position: relative; overflow: hidden;">${subSede.maps}</div>
+                        <h2 class="m-3 text-1 fs-3"><i class="bi bi-hospital"></i> <span id="sedeNombre">Sede <strong class="text-darkCyan">${subSede.nombre}</strong></span></h2>
+                        <h2 class="m-3 text-1 fs-3"><i class="bi bi-geo-alt-fill"></i> Dirección: <span id="sedeDireccion"><strong class="text-darkCyan">${subSede.direccion}</strong></span></h2>
+                        <h2 class="m-3 text-1 fs-3"><i class="bi bi-capsule"></i> Esta sede ofrece los siguientes servicios para tu <strong class="text-darkCyan">bienestar:</strong></h2>
+                    `;
+                    scrollToInfo(tituloStart);
+
+                    // Mostrar los servicios de la sub-sede seleccionada
+                    services.forEach(service => {
+                        const serviceType = service.getAttribute('data-service');
+                        if (especialidades.includes(serviceType)) {
+                            service.classList.add('visible');
+                        }
+                    });
+                }
+            });
+        });
+    } else {
           // Mostrar directamente la información de la sede sin sub-sedes
           tituloStart.innerHTML = `
               <div id="maps" style="position: relative; overflow: hidden;">${info.maps}</div>
